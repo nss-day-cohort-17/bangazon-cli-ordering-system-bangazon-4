0f075f26df49adb37ebb58a3c78d34a0d256c4d6
Ben Conner <ben.conner40@gmail.com>
2017-03-19 23:11:15 -0500

    testing for complete order and basic code to pass the tests

--- test/completeOrder-test.js
+++ test/completeOrder-test.js
@@ -1,2 +1,17 @@
-// if user enters 'Y', return payment options
-// if user enters 'N', return to main menu
+'use strict';
+
+const { assert: {isFunction} } = require('chai')
+const   { complete, notComplete} = require('../lib/completeOrder')
+
+describe('completeOrder', ()=> {
+  describe('complete',() => {
+    it('should be a function',() => {
+      isFunction(complete)
+    })
+  })
+  describe('notComplete', () => {
+    it('should be a function', () => {
+      isFunction(notComplete)
+    })
+  })
+})
